5b9b2fc6a4fd233804e741efd5ed62e9d2c05366
Funnystyle <esero@hanmail.net>
2022-02-15 04:10:07 +0900

    junit4 library 추가

--- tdd-howto-tools/bank-business/src/test/AccountTest.java
+++ tdd-howto-tools/bank-business/src/test/AccountTest.java
@@ -1,24 +1,16 @@
 package test;
 
+import org.junit.Test;
+
 import main.Account;
 
 public class AccountTest {
 
+	@Test
 	public void testAccount() throws Exception {
 		Account account = new Account();
 		if (account == null) {
 			throw new Exception("계좌생성 실패");
 		}
-	}
-	
-	public static void main(String[] args) {
-		AccountTest test = new AccountTest();
-		try {
-			test.testAccount();
-		} catch (Exception e) {
-			System.out.println("실패(X)");
-			e.printStackTrace();
-		}
-		System.out.println("성공(O)");
 	}
 }
