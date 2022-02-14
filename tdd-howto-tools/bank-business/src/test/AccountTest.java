f31feffc3f72c2b0bdd958911d49d0ec71eb4276
Funnystyle <esero@hanmail.net>
2022-02-15 04:32:31 +0900

    refactor extract method

--- tdd-howto-tools/bank-business/src/test/AccountTest.java
+++ tdd-howto-tools/bank-business/src/test/AccountTest.java
@@ -10,14 +10,18 @@ public class AccountTest {
 
 	private Account account;
 
-	@Test
-	public void testAccount() throws Exception {
+	private void setUp() {
 		account = new Account(10000);
 	}
 	
 	@Test
+	public void testAccount() throws Exception {
+		setUp();
+	}
+	
+	@Test
 	public void testGetBalance() throws Exception {
-		account = new Account(10000);
+		setUp();
 		assertEquals(10000, account.getBalance());
 
 		account = new Account(1000);
@@ -29,14 +33,14 @@ public class AccountTest {
 	
 	@Test
 	public void testDeposit() throws Exception {
-		account = new Account(10000);
+		setUp();
 		account.deposit(1000);
 		assertEquals(11000, account.getBalance());
 	}
 
 	@Test
 	public void testWithdraw() throws Exception {
-		account = new Account(10000);
+		setUp();
 		account.withdraw(1000);
 		assertEquals(9000, account.getBalance());
 	}
