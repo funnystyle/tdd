3de3b5b12341784843e06a28f860e08a0079f72c
Funnystyle <esero@hanmail.net>
2022-02-15 04:23:08 +0900

    refactoring rename

--- tdd-howto-tools/bank-business/src/main/Account.java
+++ tdd-howto-tools/bank-business/src/main/Account.java
@@ -3,8 +3,8 @@ package main;
 public class Account {
 	private int balance;
 
-	public Account(int i) {
-		this.balance = i;
+	public Account(int money) {
+		this.balance = money;
 	}
 
 	public int getBalance() {
